ace519cb09185a8a2d55db9f63c6447a02c73d66
Viktor Cef Inselberg <inselbergv@mef.edu.tr>
2018-09-13 17:58:22 +0300

    Actions method compilation file on Raspberry Pi 3
    Methods in actions: action and action_an for car movement, servo_install for servo motor troubleshoot, get_state for batch sensor reading.

---
+++ actions.py
@@ -0,0 +1,67 @@
+from picar import front_wheels, back_wheels
+from picar.SunFounder_PCA9685 import Servo
+from picar.SunFounder_PCA9685 import PCA9685
+import picar
+from time import sleep
+import numpy as np
+import sys
+from octasonic import Octasonic
+from time import time
+
+
+optimal_pace = 50
+
+        
+def action(action, fw, bw, speed, reverse = None):
+    
+    fw_possible_actions = {'forward': fw.turn_straight, 'backward': fw.turn_straight, 'left': fw.turn_left, 'right': fw.turn_right}
+    fw_position = fw_possible_actions.get(action)
+        
+    bw_position = bw.backward
+
+    if reverse and not action == 'backward':
+        bw_position = bw.forward
+    elif not reverse and action == 'backward':
+        bw_position = bw.forward
+
+    print action
+        
+    for i in range(optimal_pace):
+        fw_position()
+        bw.speed = speed
+        bw_position()   # Because the motor is working in reverse for an unknown reason
+        
+        
+def get_state(octasonic):
+    distance0 = octasonic.get_sensor_reading(2)   # faced front
+    distance1 = octasonic.get_sensor_reading(1)   # faced right
+    distance2 = octasonic.get_sensor_reading(7)   # faced left
+    #distance3 = octasonic.get_sensor_reading(0)   # faced backward
+    
+    return distance0, distance1, distance2#, distance3
+
+
+def servo_install(angle):
+    servo0 = Servo.Servo(0, bus_number=1)
+    servo1 = Servo.Servo(1, bus_number=1)
+    servo2 = Servo.Servo(2, bus_number=1)
+    servo0.write(angle)
+    servo1.write(angle)
+    servo2.write(angle)
+    
+    print "Servo set to ", angle, " degrees.\n"
+    
+    
+def action_an(angle, fw, bw, speed, reverse = None):
+            
+    if reverse:
+        bw_position = bw.forward
+    else:
+        bw_position = bw.backward
+
+    print action
+        
+    for i in range(optimal_pace):
+        fw.turn(angle)
+        bw.speed = speed
+        bw_position()   # Because the motor is working in reverse for an unknown reason
